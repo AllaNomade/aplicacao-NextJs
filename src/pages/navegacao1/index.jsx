3c3a646a34973e69e8dc3e9734115be755fe4ed9
Allan <allanpaiva17@gmail.com>
2022-09-16 10:21:21 -0300

    routes added

--- src/pages/navegacao1/index.jsx
+++ src/pages/navegacao1/index.jsx
@@ -2,7 +2,7 @@ import Layout from "../../components/Layout";
 
 const Navegação = () => {
   return(
-    <Layout titulo="Navegação em Componente #01">
+    <Layout titulo="Navegação em Componente #02">
       <h1>Navegação #02</h1>
     </Layout>
   )
